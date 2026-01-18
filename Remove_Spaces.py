06364ccf76ade5e2398fd87af80c15109bfc9806
Karthikeyan <karthikeyankumaran4@gmail.com>
2026-01-18 19:58:06 +0530

    added the two programs

---
+++ Remove_Spaces.py
@@ -0,0 +1,8 @@
+
+word ="Python is the easiest programming language "
+remove_spaces=""
+for i in word:
+    print(i)
+    if i!=" ":
+        remove_spaces =remove_spaces +i
+print(remove_spaces)
---
+++ Sum_Of_Numbers.py
@@ -0,0 +1,6 @@
+
+number = [100,200,300]
+sum =0
+for i in number:
+    sum =sum +i
+print(sum)
