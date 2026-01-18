fc836fb2066fb8eaf754306835938815d31258f3
Karthikeyan <karthikeyankumaran4@gmail.com>
2026-01-18 16:47:35 +0530

    Secon largest program is updated

--- Second_Largest_Number.py
+++ Second_Largest_Number.py
@@ -1,3 +1,14 @@
 
 # Second largest number
 
+number=[11,12,23,45,89,90,1000]
+largest=number[0]
+second_Largest=number[0]
+for num in number:
+    if num>largest:
+        second_largest=largest
+        largest=num
+print("The  largest number is:",largest)
+print("The second largest number is:",second_largest)
+
+
